d1842e3c7c596d0c656e77689b9b0c55ed4695b2
Scott Fleming <fleming@stsci.edu>
2019-10-29 18:34:23 -0400

    added first pass at flare table, some flare plots (might move), further code cleaning

--- src/uvceti/function_defs.py
+++ src/uvceti/function_defs.py
@@ -6,12 +6,18 @@
 .. moduleauthor:: Chase Million, Scott W. Fleming
 """
 
+import itertools
 import os
 from astropy.io import fits as pyfits
 from astropy import wcs as pywcs
+from astropy.stats import sigma_clip
+from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 import gPhoton
+from gPhoton import galextools as gt
+from gPhoton.gphoton_utils import read_lc
+from gPhoton.MCUtils import print_inline
 
 def listdir_contains(directory, contains_str):
     """This function returns a sorted list of files that match the given
@@ -26,15 +32,29 @@ def write_image(photon_file, band, imsz=[3600, 3600], overwrite=False,
     """This function writes an image to disk based on the photon event .csv
     file."""
     fitsfilename = photon_file.replace('csv', 'fits')
-    if os.path.exists(fitsfilename) and not overwrite:
-        print('  Reading image from {f}'.format(f=fitsfilename))
-        hdulist = pyfits.open(fitsfilename, memmap=1)
-        image = hdulist[0].data
-        wcs = pywcs.WCS(hdulist[0].header)
-    else:
+    if not os.path.exists(fitsfilename) or overwrite:
         print('  Making image {f}'.format(f=fitsfilename))
         image, wcs = make_image(photon_file, band, imsz=imsz, skypos=skypos)
         write_fits(image, wcs, fitsfilename, band, overwrite=overwrite)
+
+def write_movie(photon_file, band, tranges, stepsz=30, overwrite=False,
+                pixsz=0.000416666666666667):
+    """This function creates FITS cubes based on a photon event .csv file."""
+    fitsfilename = photon_file.replace('.csv', '-{s}s.fits'.format(
+        s=int(stepsz)))
+    if not os.path.exists(fitsfilename) or overwrite:
+        events = calibrate_photons(photon_file, band)
+        for trange in tranges:
+            print_inline(trange)
+            image, wcs = make_image(photon_file, band, trange=trange,
+                                    events=events, pixsz=pixsz)
+            try:
+                movie = np.append(movie, [image], axis=0)
+            except:
+                movie = [image]
+        hdu = pyfits.PrimaryHDU(movie)
+        hdulist = pyfits.HDUList([hdu])
+        hdulist.writeto(fitsfilename)
 
 def write_fits(image, wcs, fitsfilename, band, overwrite=False):
     """This function creates a FITS file out of a gPhoton image."""
@@ -90,7 +110,7 @@ def calibrate_photons(photon_file, band, overwrite=False):
     flat = flat[np.array(events['col'], dtype='int16'),
                 np.array(events['row'], dtype='int16')]
     events['flat'] = pd.Series(flat)
-    scale = gPhoton.galextools.compute_flat_scale(
+    scale = gt.compute_flat_scale(
         np.array(data.iloc[ix]['t'])/1000., band)
     events['scale'] = pd.Series(scale)
     response = np.array(events['flat'])*np.array(events['scale'])
@@ -133,3 +153,300 @@ def define_wcs(events, imsz=[3200, 3200], skypos=None,
     wcs.wcs.crpix = [(imsz[1]/2.) + 0.5, (imsz[0]/2.) + 0.5]
     wcs.wcs.crval = skypos
     return wcs
+
+def make_lightcurve(photon_file, band, stepsz=30., skypos=(24.76279, -17.94948),
+                    aper=gt.aper2deg(7), fixed_t0=False,
+                    makefile=False, quiet=False, filetag=''):
+    """ Generate a light curve of a specific target. """
+    lc_filename = photon_file.replace('.csv',
+                                      '-{stepsz}s{filetag}.csv'.format(
+                                          stepsz=int(stepsz), filetag=filetag))
+    if not quiet:
+        print_inline('Generating {fn}'.format(fn=lc_filename))
+    if os.path.exists(lc_filename) and not makefile:
+        if not quiet:
+            print_inline('    Pre-exists...')
+        return pd.read_csv(lc_filename)
+    events = calibrate_photons(photon_file, band)
+    skypos_recentered = recenter(events, skypos=skypos)
+    if not quiet:
+        print('Recentering aperture on [{ra}, {dec}]'.format(
+            ra=skypos_recentered[0], dec=skypos_recentered[1]))
+    ix = aper_photons(events, skypos=skypos_recentered, aper=aper)
+    if len(ix[0]) == 0:
+        return [], [], [], []
+    trange = [np.floor(np.array(events['t'])[ix].min()),
+              np.ceil(np.array(events['t'])[ix].max())]
+    if fixed_t0:
+        # Use this to force NUV and FUV to have the same bins
+        trange[0] = fixed_t0
+    expt = compute_exptime_array(np.array(events['t'].values), band, trange,
+                                 stepsz, np.array(events['flags'].values))
+    counts, tbins = [], []
+    for t0 in np.arange(trange[0], trange[1], stepsz):
+        tix = np.where((np.array(events['t'])[ix] >= t0) &
+                       (np.array(events['t']) < t0 + stepsz)[ix] &
+                       (np.array(events['flags'], dtype='int16')[ix] == 0))
+        tbins += [t0]
+        if len(tix[0]) == 0:
+            counts += [0.]
+            continue
+        counts += [np.array(events['response'])[ix][tix].sum()]
+    cps = np.array(counts) / np.array(expt)
+    cps_err = np.sqrt(counts) / np.array(expt)
+    lc = pd.DataFrame({'t0':tbins, 't1':list(np.array(tbins) + stepsz),
+                       'cps':cps, 'cps_err':cps_err,
+                       'flux':gt.counts2flux(cps, band),
+                       'flux_err':gt.counts2flux(cps_err, band),
+                       'counts':counts, 'expt':expt})
+    lc['cps_apcorrected'] = apcorrect_cps(lc, band, aper=aper)
+    lc['flux_apcorrected'] = gt.counts2flux(lc['cps_apcorrected'], band)
+    lc.to_csv(lc_filename)
+    return lc
+
+def read_lightcurve(lc_file):
+    """Reads a light curve file and returns the data as a Pandas DataFrame."""
+    if os.path.isfile(lc_file):
+        # Read in the light curve file.
+        return read_lc(lc_file)
+    else:
+        raise IOError("Light curve file not found: " + lc_file)
+
+def recenter(events, skypos=(24.76279, -17.94948),
+             aper=gt.aper2deg(7), n_iters=5):
+    """Given a position on the sky, iteratively recenter on the median photon
+    position."""
+    for i in np.arange(n_iters):
+        # iterate to recenter on the star
+        angsep = gPhoton.MCUtils.angularSeparation(skypos[0], skypos[1],
+                                                   np.array(events['ra']),
+                                                   np.array(events['dec']))
+        ix = np.where((angsep <= aper) & (np.isfinite(angsep)) &
+                      (np.array(events['flags'], dtype='int16') == 0))
+        skypos = [np.median(np.array(events['ra'])[ix]),
+                  np.median(np.array(events['dec'])[ix])]
+    return skypos
+
+def aper_photons(photon_data, skypos=(24.76279, -17.94948),
+                 aper=gt.aper2deg(7)):
+    """ Extract the events within the aperture. """
+    image = photon_data
+    angsep = gPhoton.MCUtils.angularSeparation(
+        skypos[0], skypos[1], np.array(image['ra']), np.array(image['dec']))
+    ix = np.where((angsep <= aper) & (np.isfinite(angsep)) &
+                  (np.array(image['flags'], dtype='int16') == 0))
+    return ix
+
+def compute_exptime(times, band, trange, flags):
+    """ Calculate the exposure time using the empirical formula from global
+    countrate. """
+    rawexpt = trange[1]-trange[0]
+
+    tix = np.where((times >= trange[0]) & (times < trange[1]))
+    if len(tix[0]) <= 1:
+        return 0.
+
+    # Calculate shutter
+    tfix = np.where((times >= trange[0]) & (times < trange[1]) & (flags == 0))
+    if len(tfix[0]) <= 1:
+        return 0.
+    rawexpt = max(times[tfix]) - min(times[tfix])
+    t = np.sort(np.unique(times[tfix]))
+    shutgap = 0.05
+    if len(t) <= 1:
+        return 0.
+    ix = np.where(t[1:]-t[:-1] >= shutgap)
+    shutter = np.array(t[1:]-t[:-1])[ix].sum()
+
+    # Calculate deadtime
+    band = 'FUV'
+    model = {'NUV':[-0.000434730599193, 77.217817988],
+             'FUV':[-0.000408075976406, 76.3000943221]}
+
+    rawexpt = trange[1] - trange[0] - shutter
+    gcr = len(times[tix]) / rawexpt
+    feeclkratio = 0.966
+    refrate = model[band][1] / feeclkratio
+    scr = model[band][0] * gcr + model[band][1]
+    deadtime = 1 - scr / feeclkratio / refrate
+
+    return (rawexpt - shutter) * (1. - deadtime)
+
+def compute_exptime_array(times, band, trange, stepsz, flags):
+    """ Returns exposure times as an array based on global count rates. """
+    return np.array(
+        [compute_exptime(times, band, (t0, t0 + stepsz), flags) for t0 in
+         np.arange(trange[0], trange[1], stepsz)])
+
+def apcorrect_cps(lc, band, aper=gt.aper2deg(7)):
+    """ Apply the aperture correction in units of linear counts-per-second.
+    Aperture correction is linear in magnitude units, so convert the count rate
+    into AB mag, correct it, and then convert it back.
+    """
+    return (gt.mag2counts(gt.counts2mag(lc['cps'].values, band) -
+                          gt.apcorrect1(aper, band), band))
+
+def find_flare_ranges(lc, sigma=3, quiescence=None):
+    """ Identify the start and stop indexes of a flare event. """
+    tranges = [[min(lc['t0']), max(lc['t1'])]]
+    if not quiescence:
+        q, q_err = get_inff(lc)
+    else:
+        q, q_err = quiescence
+    flare_ranges = []
+    for trange in tranges:
+        ix = np.where((np.array(lc['t0'].values) >= trange[0]) &
+                      (np.array(lc['t0'].values) <= trange[1]) &
+                      (np.array(lc['cps_apcorrected'].values) -
+                       sigma*np.array(lc['cps_err'].values) >= q))[0]
+        if not len(ix):
+            # No flares were found
+            continue
+        # This chunk extends flares until they are indistinguishable from
+        # INFF.
+        temp_ix = []
+        for ix_range in find_ix_ranges(ix):
+            # Going backwards
+            while ((lc.iloc[ix_range[0]]['cps_apcorrected'] >= q) and
+                   (ix_range[0] > 0)):
+                if (lc.iloc[ix_range[0]]['t0'] - lc.iloc[ix_range[0]-1]['t0'] >
+                        1000):
+                    break
+                ix_range = [ix_range[0] - 1] + ix_range
+            # Going forwards
+            while ((lc.iloc[ix_range[-1]]['cps_apcorrected'] >= q) and
+                   (ix_range[-1] != len(lc)-1)):
+                if (lc.iloc[ix_range[-1]+1]['t0']-lc.iloc[ix_range[-1]]['t0'] >
+                        1000):
+                    break
+                ix_range = ix_range + [ix_range[-1] + 1]
+            temp_ix += ix_range
+        ix = np.unique(temp_ix)
+        # This chunk connects gaps of a single bin width
+        temp_ix = []
+        for ix_range in find_ix_ranges(ix):
+            if ix_range[-1]+2 in ix:
+                ix_range += [ix_range[-1]+1]
+            if ix_range[0]-2 in ix:
+                ix_range += [ix_range[0]-1]
+            temp_ix += ix_range
+        ix = np.unique(temp_ix)
+        ix = ix[np.where(ix >= 0)]
+        flare_ranges += find_ix_ranges(list(np.array(ix).flatten()))
+    return flare_ranges
+
+def refine_flare_ranges(lc, makeplot=True):
+    """ Identify the start and stop indexes of a flare event after
+    refining the INFF by masking out the initial flare detection indexes. """
+    flare_ranges = find_flare_ranges(lc, sigma=3)
+    flare_ix = list(itertools.chain.from_iterable(flare_ranges))
+    quiescience_mask = [False if i in flare_ix else True for i in
+                        np.arange(len(lc['t0']))]
+    quiescence = ((lc['cps_apcorrected'][quiescience_mask] *
+                   lc['expt'][quiescience_mask]).sum() /
+                  lc['expt'][quiescience_mask].sum())
+    quiescence_err = (np.sqrt(lc['counts'][quiescience_mask].sum()) /
+                      lc['expt'][quiescience_mask].sum())
+    flare_ranges = find_flare_ranges(lc, quiescence=(quiescence,
+                                                     quiescence_err), sigma=3)
+    flare_ix = list(itertools.chain.from_iterable(flare_ranges))
+    if makeplot:
+        plt.figure(figsize=(15, 3))
+        plt.errorbar(lc['t0'], lc['cps_apcorrected'], yerr=3*lc['cps_err'],
+                     fmt='kx-')
+        plt.plot(lc['t0'].iloc[flare_ix], lc['cps_apcorrected'].iloc[flare_ix],
+                 'bo')
+        plt.hlines(quiescence, lc['t0'].min(), lc['t0'].max())
+    return flare_ranges, quiescence, quiescence_err
+
+def find_ix_ranges(ix, buffer=False):
+    """ Finds indexes in the range. """
+    foo, bar = [], []
+    for n, i in enumerate(ix):
+        if len(bar) == 0 or bar[-1] == i-1:
+            bar += [i]
+        else:
+            if buffer:
+                bar.append(min(bar)-1)
+                bar.append(max(bar)+1)
+            foo += [np.sort(bar).tolist()]
+            bar = [i]
+        if n == len(ix)-1:
+            if buffer:
+                bar.append(min(bar)-1)
+                bar.append(max(bar)+1)
+            foo += [np.sort(bar).tolist()]
+    return foo
+
+def get_inff(lc, clipsigma=3, quiet=True, band='NUV',
+             binsize=30.):
+    """ Calculates the Instantaneous Non-Flare Flux values. """
+    sclip = sigma_clip(np.array(lc['cps_apcorrected']), sigma=clipsigma)
+    inff = np.ma.median(sclip)
+    inff_err = np.sqrt(inff*len(sclip)*binsize)/(len(sclip)*binsize)
+    if inff and not quiet:
+        print('Quiescent at {m} AB mag.'.format(m=gt.counts2mag(inff, band)))
+    return inff, inff_err
+
+def calculate_flare_energy(lc, frange, distance, binsize=30, band='NUV',
+                           effective_widths={'NUV':729.94, 'FUV':255.45},
+                           quiescence=None):
+    """ Calculates the energy of a flare in erg. """
+    # TODO: interpolate over gaps
+    # TODO: model the decay phase for right-censored data
+    if not quiescence:
+        q, _ = get_inff(lc)
+    else:
+        q = quiescence[0]
+
+    # Convert from parsecs to cm
+    distance_cm = distance * 3.086e+18
+    # NOTE: This does not interpolate over small gaps.
+    if 'cps_apcorrected' in lc.keys():
+        flare_flux = (np.array(gt.counts2flux(
+            np.array(lc.iloc[frange]['cps_apcorrected']), band)) -
+                      gt.counts2flux(q, band))
+    else:
+        flare_flux = (np.array(gt.counts2flux(
+            np.array(lc.iloc[frange]['cps_apcorrected']), band)) -
+                      gt.counts2flux(q, band))
+    flare_flux = np.array([0 if f < 0 else f for f in flare_flux])
+    flare_flux_err = gt.counts2flux(np.array(lc.iloc[frange]['cps_err']), band)
+    tbins = (np.array(lc.iloc[frange]['t1'].values) -
+             np.array(lc.iloc[frange]['t0'].values))
+    integrated_flux = (binsize*flare_flux).sum()
+    """
+    GALEX effective widths from
+    http://svo2.cab.inta-csic.es/svo/theory/fps3/index.php?id=GALEX/GALEX.NUV
+    width = 729.94 A
+    http://svo2.cab.inta-csic.es/svo/theory/fps3/index.php?id=GALEX/GALEX.FUV
+    width = 255.45 A
+    """
+    fluence = integrated_flux*effective_widths[band]
+    fluence_err = (np.sqrt(((gt.counts2flux(lc.iloc[frange]['cps_err'], band) *
+                             binsize)**2).sum())*effective_widths[band])
+    energy = (4 * np.pi * (distance_cm**2) * fluence)
+    energy_err = (4 * np.pi * (distance_cm**2) * fluence_err)
+    return energy, energy_err
+
+def is_left_censored(frange):
+    """ Returns true if the light curve is cutoff on the left. """
+    return 0 in frange
+
+def is_right_censored(lc, frange):
+    """ Returns true if the light curve is cutoff on the right. """
+    return len(lc['t0'])-1 in frange
+
+def peak_flux(lc, frange):
+    """ Returns the peak flux in the light curve. """
+    return lc['flux_apcorrected'][np.argmax(np.array(lc['cps'][frange].values))]
+
+def peak_time(lc, frange, stepsz=30):
+    """ Return the bin start time corresponding to peak flux. """
+    return lc['t0'][np.argmax(np.array(lc['cps'][frange].values))] + stepsz/2
+
+def is_peak_censored(lc, frange):
+    """ Returns true is the peak flux is the first or last point in the light
+    curve. """
+    return ((np.argmax(np.array(lc['cps'][frange].values)) == 0) or
+            (np.argmax(np.array(lc['cps'][frange].values)) == len(lc) - 1))
